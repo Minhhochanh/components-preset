000ed97c02eb49bfd84383dc4a6f22a9082106b6
Minhhochanh <107168275+Minhhochanh@users.noreply.github.com>
2023-09-16 17:29:34 +0700

    Initial commit

---
+++ src/components/footer-comp.vue
@@ -0,0 +1,84 @@
+<template>
+  <body>
+    <div class="footer_container">
+      <div class="footer_script">
+        ID Screen is provided by Northwest Innovations Ltd, which is a company
+        registered in England and Wales at Companies House. Our registered
+        office is Northwest Innovations Limited, Bentley Suite, The Motorworks,
+        Chestergate, Macclesfield, Cheshire, United Kingdom, SK11 6DU and our
+        registered number is 14425558. Northwest Innovations Ltd is registered
+        for VAT under VAT Registration number GB428679449.
+      </div>
+      <div class="foot-images">
+        <img class="image-2-icon" alt="" src="./public/undefined.png" />
+        <img class="image-1-icon" alt="" src="./public/undefined1.png" />
+      </div>
+    </div>
+  </body>
+</template>
+
+<script></script>
+<style>
+.footer_container {
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  background-color: white;
+  align-items: center;
+  margin: 0 auto; /* Thiết lập margin tự động */
+  text-align: center;
+  font-family: var(--font-inter);
+  color: black;
+  justify-content: flex-start;
+  position: relative;
+  flex-shrink: 0;
+}
+.footer_script {
+  font-size: 10px;
+  margin-block-end: 1em;
+}
+.foot-images {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+.image-1-icon,
+.image-2-icon {
+  position: relative;
+  width: 89.54px;
+  height: 35.4px;
+  object-fit: cover;
+}
+.image-1-icon {
+  width: 97.17px;
+  height: 37.48px;
+  margin-inline-start: 1.5em;
+}
+body {
+  margin: 0;
+  line-height: normal;
+}
+
+:root {
+  /* fonts */
+  --font-open-sans: 'Open Sans';
+  --font-inter: Inter;
+
+  /* font sizes */
+  --font-size-xl: 20px;
+  --font-size-base: 16px;
+  --font-size-xs: 12px;
+  --font-size-3xs: 10px;
+
+  /* Colors */
+  --color-midnightblue: #18187c;
+  --color-white: #fff;
+  --color-red: #f30000;
+  --color-black: #000;
+  --color-gray: rgba(0, 0, 0, 0.8);
+  --color-gainsboro: rgba(231, 231, 231, 0.5);
+
+  /* Gaps */
+  --gap-5xl: 24px;
+}
+</style>
---
+++ src/components/header-comp.vue
@@ -0,0 +1,67 @@
+<template>
+  <body>
+    <div class="head_container">
+      <img class="head_logo" alt="" src="./public/header_logo.png" />
+      <div class="head_script_container">
+        <p class="head_script">Is your identity at risk?</p>
+        <p class="head_script">
+          25% of people in the UK will have their identity stolen. You have been
+          breached and are in danger of being a victim of fraud.
+        </p>
+      </div>
+    </div>
+  </body>
+</template>
+
+<script></script>
+<style>
+.head_container {
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  background-color: #18187c;
+  align-items: center;
+  margin: 0 auto; /* Thiết lập margin tự động */
+  text-align: center;
+  font-family: var(--font-open-sans);
+}
+.head_logo {
+  width: 50%;
+  margin-block-end: 1.5em;
+}
+.head_script_container {
+  margin: 0;
+}
+.head_script {
+  color: white;
+  font-size: 11px;
+  margin: 0;
+}
+body {
+  margin: 0;
+  line-height: normal;
+}
+
+:root {
+  /* fonts */
+  --font-open-sans: 'Open Sans';
+  --font-inter: Inter;
+
+  /* font sizes */
+  --font-size-xl: 20px;
+  --font-size-base: 16px;
+  --font-size-xs: 12px;
+  --font-size-3xs: 10px;
+
+  /* Colors */
+  --color-midnightblue: #18187c;
+  --color-white: #fff;
+  --color-red: #f30000;
+  --color-black: #000;
+  --color-gray: rgba(0, 0, 0, 0.8);
+  --color-gainsboro: rgba(231, 231, 231, 0.5);
+
+  /* Gaps */
+  --gap-5xl: 24px;
+}
+</style>
